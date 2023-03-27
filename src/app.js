ea0f133a3d96999290e62de3cc7dd47d5c84aab8
Katalin Juhász <juhaszkatalin@ktch.hu>
2023-03-27 22:50:11 +0200

    Adatok felvitele

--- src/app.js
+++ src/app.js
@@ -2,8 +2,51 @@
 File: app.js
 Author: Juhász Katalin
 Copyright: 2023, Juhász Katalin
-Group: SZOFT I/1/E
+Group: SZOFT I_1/E
 Date: 2023-03-27
 Github: https://github.com/catlina83/ships
 Licenc: GNU GPL
 */
+
+const doc = {
+    shipbody: null
+};
+const state = {
+    database: []
+}
+window.addEventListener('load', () => {
+    init();
+    getDatabase();
+    
+});
+function init() {
+    doc.shipbody = document.querySelector('#shipbody');
+}
+
+function getDatabase() {
+    let host = 'http://localhost:8000/';
+    let endpoint = 'ships';
+    let url = host + endpoint;
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {        
+        state.ships = result;
+        render();
+    });
+}
+function render() {
+    let rows = '';
+    state.ships.forEach(ship => {
+        console.log(ship.name);
+        rows += `
+            <tr>
+                <td>${ship.name}</td>
+                <td>${ship.length}</td>
+                <td>${ship.price}</td>
+                <td>${ship.person}</td>
+                <td>${ship.trailer}</td>
+            </tr>
+        `;
+    });
+    doc.shipbody.innerHTML = rows;
+}
